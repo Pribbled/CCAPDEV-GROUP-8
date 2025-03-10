b9366bff38eaa1ad240738ed7ca9d87ecd516214
carlos cailao <carloscailao@yahoo.com>
2025-03-10 13:31:39 +0800

    added profile picture to user model

--- MCO2/models/user.js
+++ MCO2/models/user.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true, trim: true},
     password: { type: String, required: true },
+    profilePicture: { type: String, default: "/common/defaultPfp.jpg" },  // New field for profile picture
     session: { type: Date, default: () => new Date(Date.now() + 3 * 7 * 24 * 60 * 60 * 1000) }, // Adds 3 weeks to expiration date
     // no need for sessions pa for phase 2
     reservations: [{ type: mongoose.Schema.Types.ObjectId, ref: "Reservation" }] // References Reservation
